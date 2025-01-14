0ec7b55b3fe036c8878b181e72ee12563e3773ba
Kristofer <kristofer@fmckl.se>
2025-01-14 23:39:03 +0100

    Preventing batch requests

--- src/ethers/hooks/useEthersProvider.ts
+++ src/ethers/hooks/useEthersProvider.ts
@@ -1,4 +1,8 @@
-import { FallbackProvider, JsonRpcProvider } from "ethers";
+import {
+  FallbackProvider,
+  JsonRpcProvider,
+  JsonRpcApiProviderOptions,
+} from "ethers";
 import { useMemo } from "react";
 import type { Chain, Client, Transport } from "viem";
 import { type Config, useClient } from "wagmi";
@@ -13,12 +17,16 @@ export function clientToProvider(client?: Client<Transport, Chain>) {
   };
   if (transport.type === "fallback") {
     const providers = (transport.transports as ReturnType<Transport>[]).map(
-      ({ value }) => new JsonRpcProvider(value?.url, network)
+      ({ value }) => new JsonRpcProvider(value?.url, network),
     );
     if (providers.length === 1) return providers[0];
     return new FallbackProvider(providers);
   }
-  return new JsonRpcProvider(transport.url, network);
+  const options: JsonRpcApiProviderOptions = {
+    batchMaxCount: 1,
+  };
+  console.log(transport.url, network);
+  return new JsonRpcProvider(transport.url, network, options);
 }
 
 /** Action to convert a viem Client to an ethers.js Provider. */
